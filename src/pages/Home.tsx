a38b3d2a77e4c2ab8825ceecabb6b8585c3e40e4
Gabfuwak <gabin.maury0@gmail.com>
2026-01-28 21:27:08 +0100

    slop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome</h1>
-      <p>Welcome to my website! I document my projects and experiments here in robotics, AI, computer graphics and more in the future. This serves as a portfolio but feel free to use any code or methods, as everything I share is opensource under the MIT licence. If you want to explore my computer graphics experiments I would recommend activating WebGPU flags in your browser (and use chrome if you're running linux).</p>
+      <p>Welcome to my website! I document my projects and experiments here in robotics, AI, computer graphics and more in the future. This serves as a portfolio but feel free to use any code or methods. If you want to explore my computer graphics experiments I would recommend activating WebGPU flags in your browser (and use chrome if you're running linux).</p>
       <h1>About me</h1>
       I'm a CS student in France, I'm mostly interested in robotics, computer graphics, deep learning and computer vision but overall any subject that impacts the world around us in a way or another.
 
